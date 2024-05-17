cabb471b773b5b232981d2989c5f62d9065834c6
EC2 Default User <ec2-user@ip-172-31-3-247.ap-northeast-1.compute.internal>
2024-05-17 10:53:34 +0000

    検索機能　ほぼ完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,26 @@ class User < ApplicationRecord
 
   has_one_attached :image
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [100, 100]).processed
+    image.variant(resize_to_limit:[width, height]).processed
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+  
 end
