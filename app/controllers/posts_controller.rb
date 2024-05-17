29b22f4b428aeedc265950bfef1fb71de9e0ab86
EC2 Default User <ec2-user@ip-172-31-3-247.ap-northeast-1.compute.internal>
2024-05-17 13:51:18 +0000

    コメント機能　ほぼ完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,7 @@ class PostsController < ApplicationController
     # is_matching_login_user
     @post = Post.find(params[:id])
     @user = @post.user
+    @post_comment = PostComment.new
   end
 
   def edit
