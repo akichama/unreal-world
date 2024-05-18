af4dc2d97b8f8d0403ff3a2682d7c50f1d3ca20e
EC2 Default User <ec2-user@ip-172-31-3-247.ap-northeast-1.compute.internal>
2024-05-18 12:53:18 +0000

    コメント機能　完成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,11 @@ class Post < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_one_attached :image
   belongs_to :user
-  
+
   validates :title, presence: true
   validates :body, presence: true
   validates :image, presence: true
-  
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -14,7 +14,31 @@ class Post < ApplicationRecord
     end
     image.variant(resize_to_limit: [100, 100]).processed
   end
-  
+
+  def get_show_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def get_index_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def get_mypage_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def self.looks(search, word)
     if search == "perfect_match"
       @post = Post.where("title LIKE?","#{word}")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.find_or_create_by(email: 'admin@exmaple.com') do |a|
+  a.password = 'passw@rd'
+end
